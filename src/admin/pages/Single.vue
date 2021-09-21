8f8fa26f447860aea1b5c39ecbca570bad1227ac
bdtanbir <tanbirsylhet20@gmail.com>
2021-09-21 15:02:51 +0600

    working on single

--- src/admin/pages/Single.vue
+++ src/admin/pages/Single.vue
@@ -1,20 +1,68 @@
 <template>
     <div class="ffb-single-wrap">
         <router-link to="/" class="back-to-home-btn">Back</router-link>
-        <h1>I am From Single = {{tableInfos.id}}</h1>
+        <h1>{{tableInfos.title}}</h1>
+        <form @submit.prevent="updateTable">
+            <div class="input-group">
+                <label for="upd_title">
+                    Title
+                </label>
+                <input type="text" id="upd_title" ref="upd_title" :value="tableInfos.title">
+            </div>
+            <div class="input-group">
+                <label for="upd_tags">
+                    Tags
+                </label>
+                <input type="text" id="upd_tags" ref="upd_tags" :value="tableInfos.tags">
+            </div>
+            <div class="input-group">
+                <label for="upd_description">
+                    Description
+                </label>
+                <textarea name="upd_description" id="upd_description" :value="tableInfos.description"></textarea>
+            </div>
+            <div class="input-group ffb-single-update-btn">
+                <button>Update</button>
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
 export default {
+    name: 'Single',
     data() {
         return {
-            tableInfos: this.$route.params.item ? this.$route.params.item : {}
+            upd_title: '',
+            tableInfos: this.$route.params.item ? this.$route.params.item : {},
         }
-    }
+    },
+    methods: {
+        updateTable() {
+            const title = this.$refs.upd_title
+            console.log('updated: '+title.value);
+        }
+    },
 }
 </script>
 
 <style>
-    
+    .ffb-single-wrap {
+        margin-top: 15px;
+        background: #fff;
+        border-radius: 4px;
+        box-shadow: 0 0 30px rgb(0 0 0 / 2%);
+        padding: 20px 20px 20px 20px;
+    }
+    .ffb-single-wrap .back-to-home-btn {
+        display: inline-block;
+        background: #fff;
+        text-decoration: none;
+        color: #000;
+        padding: 4px 10px;
+        border-radius: 4px;
+        transition: .3s;
+        box-shadow: 2px 2px 0px rgb(0 0 0 / 15%);
+        border: 1px solid #eee;
+    }
 </style>
